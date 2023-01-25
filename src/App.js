9869d6e5f0570f0ddf89f48203bfde08e0206f50
Karol <K_wlodarczyk@interia.pl>
2023-01-25 23:24:34 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './pages/Home.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/search'>
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Routes>
+      </Router>
+   </div>
   );
 }
 
